cbb01215450a91ac0148026e76a7dfb53fc4362d
Cadu Di Alessandro <cadurpo@gmail.com>
2021-08-09 10:22:51 -0300

    atualização de dados de responsividade

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
-function popupToggle(){
-    const newsletter = document.getElementById('newsletter');
-    newsletter.classList.toggle('active');
+const popup = document.querySelector('.popup');
+const close = document.querySelector('.close');
+
+window.onload = function(){
+    
+    setTimeout(function(){
+        popup.style.display = 'block';//adicionando tempo de delay para o popup
+    }, 2000)
 }
+
+close.addEventListener('click', () => {
+    popup.style.display = 'none';
+})
